1643829a04ecbb2e852a4bfff5369bcd8179c2ab
nazar <nazarisayev7@gmail.com>
2018-07-18 16:03:44 +1000

    Add Article list module.

---
+++ react_redux_app/src/ListPagination.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import agent from '../agent';
+import { connect } from 'react-redux';
+import { SET_PAGE } from '../constants/actionTypes';
+
+const mapDispatchToProps = dispatch => ({
+  onSetPage: (page, payload) =>
+    dispatch({ type: SET_PAGE, page, payload })
+});
+
+const ListPagination = props => {
+  if (props.articlesCount <= 10) {
+    return null;
+  }
+
+  return (
+    <nav>
+      <ul className="pagination">
+
+        {
+          range.map(v => {
+            const isCurrent = v === props.currentPage;
+            const onClick = ev => {
+              ev.preventDefault();
+              setPage(v);
+            };
+            return (
+              <li
+                className={ isCurrent ? 'page-item active' : 'page-item' }
+                onClick={onClick}
+                key={v.toString()}>
+
+                <a className="page-link" href="">{v + 1}</a>
+
+              </li>
+            );
+          })
+        }
+
+      </ul>
+    </nav>
+  );
+};
+
+export default connect(() => ({}), mapDispatchToProps)(ListPagination);
